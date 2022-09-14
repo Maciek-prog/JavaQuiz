d96fdf292ae9022a42adda6cb856947df39eec86
Maciek-prog <pretkimaciej1@gmail.com>
2022-09-14 19:55:27 +0200

    services

--- src/main/java/pl/quizJava/Quiz/StartupRunner.java
+++ src/main/java/pl/quizJava/Quiz/StartupRunner.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.quizJava.Quiz.database.entities.PlayerEntity;
 import pl.quizJava.Quiz.database.entities.repositories.PlayerRepository;
+import services.QuizDataService;
 
 
 import java.util.List;
@@ -15,9 +16,13 @@ public class StartupRunner implements CommandLineRunner {
 
     @Autowired
     private PlayerRepository playerRepository;
+    @Autowired
+    private QuizDataService quizDataService;
 
     @Override
     public void run(String...args) throws Exception {
+        log.info("Executing startup actions...");
+        quizDataService.getQuizCategories();
         log.info("Executing startup actions...");
         playerRepository.save(new PlayerEntity("John"));
         playerRepository.save(new PlayerEntity("Harry"));
